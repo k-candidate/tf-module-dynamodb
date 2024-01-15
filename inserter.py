78bf90e16ae954d2699dc1c9859c95243e3e1082
k-candidate <k-candidate@outlook.com>
2024-01-15 02:48:12 +0100

    fix: troubleshoot lambda

--- inserter.py
+++ inserter.py
@@ -5,14 +5,17 @@ region_name = "us-east-1"
 
 dynamodb = boto3.client("dynamodb", endpoint_url=endpoint_url, region_name=region_name)
 
-ssm = boto3.client('ssm')
-parameter = ssm.get_parameter(Name='/dev/table_name')
+ssm = boto3.client('ssm',endpoint_url=endpoint_url)
+parameter = ssm.get_parameter(Name='/dev/table_name')['Parameter']['Value']
 
-print(parameter['Parameter']['Value'])
+print(parameter)
 
 def insert_data(event, context):
     for record in event['Records']:
-        print("test")
-        payload = record["body"]
-        print(str(payload))
-        dynamodb.put_item(TableName=parameter['Parameter']['Value'], Item=payload)
+        try:
+            print("test")
+            payload = record["body"]
+            print(str(payload))
+            dynamodb.put_item(TableName=parameter, Item=payload)
+        except Exception as e:
+            print(e)
